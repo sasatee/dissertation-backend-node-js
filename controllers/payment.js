938150f2fe5d7a0ca74e0f66f303a46742967d61
trainee <trainee@zapproach.com>
2024-05-11 02:01:09 +0400

    mur added in payment controller to accepted rupees

--- controllers/payment.js
+++ controllers/payment.js
@@ -5,7 +5,7 @@ const paymentIntent = async (req, res) => {
  try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: req.body.amount,
-      currency: 'usd',
+      currency: 'mur',
       automatic_payment_methods: {
         enabled: true,
       },
