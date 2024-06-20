8aa931ebf02f2e29a611f5287b0e0872c420bf6a
trainee <trainee@zapproach.com>
2024-06-20 15:14:28 +0400

    email nodemailer implement

--- util/email.js
+++ util/email.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
+// const sendEmailer = async (option) => {
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     auth: {
+//       user: process.env.EMAIL_FOR_USER,
+//       pass: process.env.EMAIL_PASSWROD,
+//     },
+//   });
+
+
 const sendEmailer = async (option) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    service: "gmail",
     auth: {
-      user: process.env.EMAIL_FOR_USER,
+      user:  process.env.EMAIL_HOST,
       pass: process.env.EMAIL_PASSWROD,
     },
+    tls:{
+      rejectUnauthorized:false
+    }
   });
 
   //defined emai option
