8b1e9ed3e9b36e12473d8b4888e29cc2eba991e5
trainee <trainee@zapproach.com>
2024-06-21 13:26:25 +0400

    100000 * Math.random() * 10 cahnge

--- util/Verification.js
+++ util/Verification.js
@@ -2,7 +2,7 @@ const crypto = require("crypto");
 
 const generateVerificationToken = () => {
   // const verifytoken = crypto.randomBytes(32).toString("hex");
-  const verifytoken = Math.floor(1000000 * Math.random() * 10).toString();
+  const verifytoken = Math.floor(100000 * Math.random() * 10).toString();
   const hashedToken = crypto
     .createHash("sha256")
     .update(verifytoken)
